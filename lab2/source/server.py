3655a701510a680bf602ac32321ea69920ab44ab
Krystian Bendinger <krystian.bendinger@gmail.com>
2022-11-19 11:46:35 +0100

    add parsing query

--- lab2/source/server.py
+++ lab2/source/server.py
@@ -3,25 +3,28 @@ import http.server
 import socketserver
 import os
 from datetime import datetime
+from urllib.parse import urlparse, parse_qs
 
 #print('source code for "http.server":', http.server.__file__)
 
 class web_server(http.server.SimpleHTTPRequestHandler):
     
     def do_GET(self):
-
-        print(self.path)
+ 
+        query_params = parse_qs(urlparse(self.path).query) 
         
-        if self.path == '/':
-            self.protocol_version = 'HTTP/1.1'
-            self.send_response(200)
-            self.send_header("Content-type", "text/html; charset=UTF-8")
-            self.end_headers()            
+        # if self.path == '/':
+        self.protocol_version = 'HTTP/1.1'
+        self.send_response(200)
+        self.send_header("Content-type", "text/html; charset=UTF-8")
+        self.end_headers()
+        if not query_params:            
             self.wfile.write(b"Hello World!\n")
+        elif(query_params['cmd'] == ['time']):
             self.wfile.write(str.encode(datetime.now().strftime("%H:%M:%S")))
 
-        else:
-            super().do_GET()
+        # else:
+        #     super().do_GET()
     
 # --- main ---
 
