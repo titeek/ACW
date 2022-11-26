73aeefd7fd1bbdc292896b57a7688a33dfe1325a
Krystian Bendinger <krystian.bendinger@gmail.com>
2022-11-26 10:52:38 +0100

    add simple str param logic

--- lab3/source/server.py
+++ lab3/source/server.py
@@ -13,20 +13,18 @@ class web_server(http.server.SimpleHTTPRequestHandler):
  
         path = urlparse(self.path)
         query_params = parse_qs(path.query) 
+        document = query_params.get('str', None)[0]
         
         if path.path == '/':
             self.protocol_version = 'HTTP/1.1'
             self.send_response(200)
             self.send_header("Content-type", "text/html; charset=UTF-8")
             self.end_headers()
-            if not query_params:            
-                self.wfile.write(b"Hello World!\n")
-            elif(query_params.get('cmd', None) == ['time']):
-                self.wfile.write(str.encode(datetime.now().strftime("%H:%M:%S")))
-            elif(query_params.get('cmd', None) == ['rev']):
-                string_to_reverse = query_params.get('str', None)[0]
-                if string_to_reverse:
-                    self.wfile.write(str.encode(string_to_reverse[::-1]))
+
+            if document:
+                for char in document:
+                    self.wfile.write(str.encode(char))
+                    
         else:
             super().do_GET()
     
